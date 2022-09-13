2f4de094b056205ee9c017839b463da0661f8655
Ray Deck <ray@raydeck.com>
2022-09-13 16:21:35 -0400

    fix: clean up noneviltoken

--- hardhat/contracts/CantBeEvil.sol
+++ hardhat/contracts/CantBeEvil.sol
@@ -8,17 +8,34 @@ import {LicenseVersion, CantBeEvil} from "@a16z/contracts/licenses/CantBeEvil.so
 import {TermsableNoToken} from "@polydocs/contracts/contracts/termsable/TermsableNoToken.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 // this contract inherits getLicenseURI() and getLicenseName() from the CantBeEvil contract
 
-contract NonEvilToken is ERC721, Ownable, CantBeEvil, TermsableNoToken {
+contract NonEvilToken is
+    Ownable,
+    CantBeEvil,
+    TermsableNoToken,
+    ERC721URIStorage,
+    ERC2981
+{
     using Strings for uint256;
     string RENDERER = "https://hellosign-bridge.polydocs.xyz";
 
-    constructor(LicenseVersion _version, address _newOwner)
-        ERC721("HelloToken", "NET")
-        CantBeEvil(_version)
-    {
+    using Counters for Counters.Counter;
+    Counters.Counter public _tokenIds; // Changed to public to test for the timebeing
+    string private _uri;
+    event MintNFT(address sender, uint256 tokenId);
+    mapping(address => bool) private _minters;
+
+    constructor(
+        LicenseVersion _version,
+        address _newOwner,
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) CantBeEvil(_version) {
         _setGlobalRenderer(RENDERER);
         _setGlobalTemplate(
             string.concat(_BASE_LICENSE_URI, uint256(licenseVersion).toString())
@@ -31,7 +48,7 @@ contract NonEvilToken is ERC721, Ownable, CantBeEvil, TermsableNoToken {
         public
         view
         virtual
-        override(CantBeEvil, ERC721)
+        override(CantBeEvil, ERC721, ERC2981)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
@@ -73,5 +90,39 @@ contract NonEvilToken is ERC721, Ownable, CantBeEvil, TermsableNoToken {
         // address _checkedSigner = ECDSA.recover(hash, _signature);
         // require(_checkedSigner == _signer);
         _acceptTerms(_signer, _signatureUrl);
+    }
+
+    function mint(string memory _tokenURI)
+        public
+        onlyMetaSigner
+        returns (uint256)
+    {
+        require(_acceptedTerms(_msgSender()), "Terms not accepted");
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, _tokenURI);
+
+        _tokenIds.increment();
+
+        emit MintNFT(msg.sender, newItemId);
+        return newItemId;
+    }
+
+    function mintFor(string memory _tokenURI, address _to)
+        public
+        onlyMetaSigner
+        returns (uint256)
+    {
+        //require(isMinter(_to), "Not a minter");
+        require(_acceptedTerms(_to), "Terms not accepted");
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(_to, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+        _tokenIds.increment();
+        emit MintNFT(_to, newItemId);
+        return newItemId;
     }
 }
