ac819a66ef255f0e6623fa711eed1e4ca5e74ca5
Ray Deck <ray@raydeck.com>
2022-09-17 16:36:49 -0400

    feat: UI cleanup for dropfile (pulsing on upload, etc)

--- composer-app/src/DropFile.tsx
+++ composer-app/src/DropFile.tsx
@@ -70,7 +70,7 @@ export const DropFileBase: FC<{
       >
         <div className="space-y-1 text-center">
           {isUploading && (
-            <CloudUploadIcon className="mx-auto h-12 w-12 text-gray-400 object-cover animated-pulse" />
+            <CloudUploadIcon className="mx-auto h-12 w-12 text-gray-400 object-cover animate-pulse" />
           )}
 
           {!isUploading && !value && (
@@ -114,23 +114,25 @@ export const DropFileBase: FC<{
             </Fragment>
           )}
           {!isUploading && (
-            <div className="flex text-sm text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-              >
-                <span>{"Upload a file"}</span>
-                <input
-                  name={name}
-                  type="file"
-                  className="sr-only"
-                  {...getInputProps()}
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
+            <div>
+              <div className="flex text-sm text-gray-600">
+                <label
+                  htmlFor="file-upload"
+                  className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                >
+                  <span>{"Upload a file"}</span>
+                  <input
+                    name={name}
+                    type="file"
+                    className="sr-only"
+                    {...getInputProps()}
+                  />
+                </label>
+                <p className="pl-1">or drag and drop</p>
+              </div>
+              <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
             </div>
           )}
-          <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
         </div>
       </div>
     </div>
